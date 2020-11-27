58f7b2fe2c84ece5bf5104a0bf0aa49247690678
vadillahnurhikmah <vadillahnurhikmah256@gmail.com>
2020-11-27 20:45:40 +0700

    My Navigation

---
+++ app/src/main/java/com/dila/mynavigation/ProfieActivity.kt
@@ -0,0 +1,11 @@
+package com.dila.mynavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profie)
+    }
+}
